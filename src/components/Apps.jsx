8260b66bccf010c8ae7efd25394b73161a95465d
Lilit Yepremyan <lilit.epremyan.96@gmail.com>
2021-08-06 15:02:05 +0400

    feat: add infinit scroll

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -2,30 +2,31 @@ import { useEffect, useState } from 'react';
 import Movie from './Movie/Movie';
 
 const FEATURED_API =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 // const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
-// const API_URL =
-//   'https://api.themoviedb.org/3/discover/movie?api_key=a9b4a343adf7d98ac7614d76c835e0ea&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${p}&with_watch_monetization_types=flatrate';
-
 function Apps() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    getMovies(FEATURED_API);
-  }, []);
+    getMovies(FEATURED_API + page);
+  }, [page]);
 
-    const getMovies = (API) => {
-      fetch(FEATURED_API)
-        .then((res) => res.json())
-        .then((data) => {
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        if (page === 1) {
           setMovies(data.results);
-        });
-    
-  }
+        } else {
+          setMovies([...movies, ...data.results]);
+        }
+      });
+  };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -43,6 +44,20 @@ function Apps() {
   const handleOnChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  const scrollHandler = (event) => {
+    if (
+      event.target.documentElement.scrollHeight <
+      event.target.documentElement.scrollTop + window.innerHeight + 3
+    ) {
+      setPage((p) => p + 1);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return () => document.removeEventListener('scroll', scrollHandler);
+  }, []);
 
   return (
     <>
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -13,19 +13,16 @@ const setVoteClass = (vote) => {
   }
 };
 
+
+
 const Movie = ({ data }) => {
   const { title, poster_path, overview, vote_average } = data;
+  const src = poster_path
+    ? IMG_API + poster_path
+    : "https://azbukadekor.ru/upload/iblock/d7f/d7f9ddfa2750b6fa4a35bbef509c32d4.jpg";
   return (
     <div className="movie">
-      <img
-        src={
-          poster_path
-            ? IMG_API + poster_path
-            : 'https://www.google.com/search?q=%D0%B4%D1%83%D0%B1%D0%BB%D1%8C+%D1%80%D0%B8%D1%81%D1%83%D0%BD%D0%BE%D0%BA&tbm=isch&hl=ru&sa=X&ved=2ahUKEwjYtKj13ZryAhWKmKQKHTiaBeEQrNwCKAB6BQgBELgB&biw=1439&bih=688#imgrc=pmoppNQnkk_IpM&imgdii=UdBlOjrZoV2ubM'
-        }
-        width="300px"
-        alt={title}
-      ></img>
+      <img src={src} width="300px" alt={title}></img>
       <div className="movie-info">
         <h3>{title}</h3>
         <span className={`tag ${setVoteClass(vote_average)}`}>
