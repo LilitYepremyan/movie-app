a7539bde1783ae5331ce22c58c569ad7d0fb6d77
Lilit Yepremyan <lilit.epremyan.96@gmail.com>
2021-08-06 18:22:37 +0400

    feat: add favorite star icon

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -3,36 +3,21 @@ import './Movie.css';
 
 const IMG_API = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
 
-const setVoteClass = (vote) => {
-  if (vote > 8) {
-    return 'green';
-  } else if (vote >= 6) {
-    return 'orange';
-  } else {
-    return 'red';
-  }
-};
-
-
-
 const Movie = ({ data }) => {
-  const { title, poster_path, overview, vote_average } = data;
+  const { title, poster_path, overview } = data;
   const src = poster_path
     ? IMG_API + poster_path
-    : "https://azbukadekor.ru/upload/iblock/d7f/d7f9ddfa2750b6fa4a35bbef509c32d4.jpg";
+    : 'https://azbukadekor.ru/upload/iblock/d7f/d7f9ddfa2750b6fa4a35bbef509c32d4.jpg';
   return (
     <div className="movie">
       <img src={src} width="300px" alt={title}></img>
       <div className="movie-info">
         <h3>{title}</h3>
-        <span className={`tag ${setVoteClass(vote_average)}`}>
-          {vote_average}
-        </span>
-        {/* <p>{overview}</p> */}
       </div>
       <div className="movie-over">
         <h3>Overview:</h3>
         <p>{overview}</p>
+        <span class="fa fa-star checked fa-2x"></span>
       </div>
     </div>
   );
